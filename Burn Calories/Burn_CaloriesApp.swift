4c1812a928d2c78d569d0851ca66b15f5a554eb0
rong <borong1002@gmail.com>
2022-05-26 00:06:53 +0800

    Initial Commit

---
+++ Burn Calories/Burn_CaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Burn_CaloriesApp.swift
+//  Burn Calories
+//
+//  Created by Lin Bo Rong on 2022/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Burn_CaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Burn Calories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Burn Calories
+//
+//  Created by Lin Bo Rong on 2022/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
